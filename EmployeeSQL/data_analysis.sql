f32d15bb732a262657d14214b47cd030e2869191
danieltp92 <daniel.tp92@gmail.com>
2021-03-06 00:06:46 -0600

    update with second point of data analysis

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -7,3 +7,15 @@ FROM Employees as A
 INNER JOIN Salaries as B
 ON A.emp_no = B.emp_no
 ;
+
+--------------------------------------------
+--------------------------------------------
+--------------------------------------------
+
+
+SELECT 	A.first_name, 
+		A.last_name, 
+		A.hire_date
+FROM Employees as A
+WHERE SUBSTR(CAST(A.hire_date AS TEXT),1,4) = '1986'
+;
