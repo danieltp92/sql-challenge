aa83a94e82eec2e16b8b4c5feb8bf48e015547f0
danieltp92 <daniel.tp92@gmail.com>
2021-03-06 00:18:35 -0600

    update with third point of data analysis

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -12,10 +12,25 @@ ON A.emp_no = B.emp_no
 --------------------------------------------
 --------------------------------------------
 
-
 SELECT 	A.first_name, 
 		A.last_name, 
 		A.hire_date
 FROM Employees as A
 WHERE SUBSTR(CAST(A.hire_date AS TEXT),1,4) = '1986'
+;
+
+--------------------------------------------
+--------------------------------------------
+--------------------------------------------
+
+SELECT 	A.depto_no, 
+		B.dept_name, 
+		A.emp_no, 
+		C.last_name, 
+		C.first_name
+FROM Dept_Manager as A
+INNER JOIN Departments as B
+ON A.depto_no = B.depto_no
+INNER JOIN Employees as C
+ON A.emp_no = C.emp_no
 ;
